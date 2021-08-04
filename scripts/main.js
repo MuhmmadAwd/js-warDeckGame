a340713602585721acb3dff96257ccfaa088c353
mohmed2000 <noza25588@gmail.com>
2021-08-04 11:34:10 +0300

    notes-form-teamleader

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,7 @@ class CardEngine {
     this.Player2Cards = [];
     this.GroundCards = [];
     this.Result = null;
-    this.turn = 1;
-    this.lastPlayerCard = [];
+    this.turn = true;
   }
   init = (e) => {
     let player1 = document.querySelector(".player1Img");
@@ -46,46 +45,48 @@ class CardEngine {
     this.Player2Cards = this.deck.slice(this.deck.length / 2);
   };
   OnSetPlayer1Turn = () => {
-    if (this.turn == 1) {
-      this.Result.innerHTML = "";
-      this.shiftPlayerCardToGround(this.Player1Cards);
-      this.turn = 2;
+    if (this.turn !== true) {
+      this.Result.innerHTML = "it's not your turn";
       return;
     }
-    this.Result.innerHTML = "it's not your turn";
+    this.Result.innerHTML = "";
+    this.shiftPlayerCardToGround(this.Player1Cards);
+    this.turn = false;
   };
   OnSetPlayer2Turn = () => {
-    if (this.turn == 2) {
-      this.Result.innerHTML = "";
-      this.shiftPlayerCardToGround(this.Player2Cards);
-      this.turn = 1;
+    if (this.turn !== false) {
+      this.Result.innerHTML = "it's not your turn";
       return;
     }
-    this.Result.innerHTML = "it's not your turn";
-  };
-  shiftPlayerCardToGround = (PlayerCards) => {
-    this.lastPlayerCard = PlayerCards.pop();
-    this.GroundCards.push(this.lastPlayerCard);
-    this.showImg();
-    this.FillGround(PlayerCards);
-    this.CountCardsNumber();
-    this.CheckWin();
+    this.Result.innerHTML = "";
+    this.shiftPlayerCardToGround(this.Player2Cards);
     this.turn = true;
   };
-
-  FillGround = (PlayerCards) => {
-    if (this.GroundCards.length > 1) {
-      let lastCard = this.GroundCards[this.GroundCards.length - 2];
-      if (lastCard.number === this.lastPlayerCard.number) {
-        this.Player1Cards = PlayerCards.concat(this.GroundCards);
-        this.GroundCards = [];
-      }
-    }
+  shiftPlayerCardToGround = (PlayerCards) => {
+    let lastPlayerCard = PlayerCards.pop();
+    this.GroundCards.push(lastPlayerCard);
+    this.showImg(lastPlayerCard);
+    this.FillGround(lastPlayerCard, PlayerCards);
+    this.CountCardsNumber();
+    this.CheckWin();
   };
-  showImg = () => {
+
+  showImg = (lastPlayerCard) => {
     let img = document.querySelector(".card-img"); //line 21 in html
     img.classList.add("displayCard");
-    img.src = `./img/${this.lastPlayerCard.type}${this.lastPlayerCard.number}.png`;
+    img.src = `./img/${lastPlayerCard.type}${lastPlayerCard.number}.png`;
+  };
+
+  FillGround = (lastPlayerCard, PlayerCards) => {
+    if (this.GroundCards.length < 1) {
+      return;
+    }
+    let lastCard = this.GroundCards[this.GroundCards.length - 2];
+    if (lastCard.number !== lastPlayerCard.number) {
+      return;
+    }
+    this.Player1Cards = PlayerCards.concat(this.GroundCards);
+    this.GroundCards = [];
   };
   CountCardsNumber = () => {
     let cardNumPlayer1 = document.querySelector(".cardNumPlayer1");
@@ -94,13 +95,22 @@ class CardEngine {
     cardNumPlayer2.innerHTML = this.Player2Cards.length;
   };
   CheckWin = () => {
-    if (this.Player1Cards.length < 1 || this.Player2Cards.length < 1) {
-      this.Result.innerHTML = " مليون مبروك ";
-      let myAudioElement = new Audio("./win.mp3");
-      myAudioElement.addEventListener("canplaythrough", (e) => {
-        myAudioElement.play();
-      });
+    if (this.Player2Cards.length > 0) {
+      return;
     }
+    this.singForWinner();
+
+    if (this.Player1Cards.length > 0) {
+      return;
+    }
+    this.singForWinner();
+  };
+  singForWinner = () => {
+    this.Result.innerHTML = " مليون مبروك ";
+    let myAudioElement = new Audio("./win.mp3");
+    myAudioElement.addEventListener("canplaythrough", (e) => {
+      myAudioElement.play();
+    });
   };
 } //CardEngine End
 let card = new CardEngine();
